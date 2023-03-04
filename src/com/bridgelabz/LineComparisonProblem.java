ca674e7be62ac302c18627a7b3282bb5d48a0e66
drisyaathul <drisyak809@gmail.com>
2023-03-04 21:58:39 +0800

    Added: Equality of Two Lines Using Equals Method

--- src/com/bridgelabz/LineComparisonProblem.java
+++ src/com/bridgelabz/LineComparisonProblem.java
@@ -1,11 +1,12 @@
 package com.bridgelabz;
 
 import java.util.Scanner;
-
 public class LineComparisonProblem {
     public static void main(String[] args) {
         System.out.println("**** Welcome to Line Comparison Computation Program ****");
 
+        double firstLine;
+        double secondLine;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the value of x1 : ");
         int x1 = scanner.nextInt();
@@ -16,10 +17,30 @@ public class LineComparisonProblem {
         System.out.println("Enter the value of y2 : ");
         int y2 = scanner.nextInt();
 
-        double length = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+        firstLine = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+        System.out.println("Length of a First Line : "+firstLine);
+        System.out.println();
 
-        System.out.println("Length of a Line : "+length);
+        System.out.println("Enter the value of x3 : ");
+        int x3 = scanner.nextInt();
+        System.out.println("Enter the value of y3 : ");
+        int y3 = scanner.nextInt();
+        System.out.println("Enter the value of x4 : ");
+        int x4 = scanner.nextInt();
+        System.out.println("Enter the value of y4 : ");
+        int y4 = scanner.nextInt();
 
+        secondLine = Math.sqrt(Math.pow(x4-x3,2) + Math.pow(y4-y3,2));
+        System.out.println("Length of a Second Line : "+secondLine);
+        System.out.println();
 
+        String line1 = String.valueOf(firstLine);
+        String line2 = String.valueOf(secondLine);
+        boolean result = line1.equals(line2);
+
+        if (result == true){
+            System.out.println("Length of Two Lines are EQUAL");
+        }else
+            System.out.println("Length of Two Lines are not EQUAL");
     }
 }
